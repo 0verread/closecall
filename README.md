ac513216fbdcf19979a86711806c92abf533d480
sub0ne1 <iamsubhajit.d@gmail.com>
2024-07-17 21:10:09 -0400

    a few update

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-## tsuper
+## closecall
 
 a lightweight npm package to keep llm calls within the relevant keywords.
 
+### Use case
+
+AI-powered chatbots can be abused with irrelevant queries, making LLM cost higher and expensive. Use this simple package to keep your LLM calls within relevant keywords. 
+
+### Usage
+
+### Contribution
+
+### License
+
+under [MIT](LICENSE) License
--- src/index.ts
+++ src/index.ts
@@ -4,41 +4,15 @@ env.allowLocalModels = false;
 env.useBrowserCache = false;
 
 const MODEL = "Xenova/mobilebert-uncased-mnli";
-const TASK = "zero-shot-classification";
 
-const pipelinePromise = pipeline(TASK, MODEL, { quantized: true }).then((pipe) => {
+const pipelinePromise = pipeline("zero-shot-classification", MODEL, { quantized: true }).then((pipe) => {
   return pipe;
 });
 
-
-
-class ClassificationPipeline {
-  static instance: any = null;
-
-  static async getInstance(
-    progress_callback: Function | undefined = undefined
-  ) {
-    if (this.instance === null) {
-      this.instance = await pipelinePromise;
-    }
-    return this.instance;
-  }
+const initInstance = async () => {
+  let instance  = await pipelinePromise;
+  return instance;
 }
 
-// Function to check relevance
-const isRelevant = async (
-  prompt: string,
-  keywords: string[]
-): Promise<boolean> => {
-  const classifier = await ClassificationPipeline.getInstance();
-  const sequence_to_classify = prompt;
-  const candidate_labels = [`${keywords.join(", or ")}.`, `something else`];
-  const result = await classifier(sequence_to_classify, candidate_labels);
 
-  const irrelevantIndex = result.labels.indexOf("something else");
-  const relevantIndex = irrelevantIndex === 0 ? 1 : 0;
 
-  return result.scores[relevantIndex] > result.scores[irrelevantIndex];
-}
-
-export default isRelevant;
